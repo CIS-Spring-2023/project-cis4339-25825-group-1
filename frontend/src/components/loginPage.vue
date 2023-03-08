7911bcb8d93a46401605eccdf6f310c6e79cc8f4
BryanHTo <bryanhto@gmail.com>
2023-03-07 18:32:24 -0600

    Added in LoginPage

---
+++ frontend/src/components/loginPage.vue
@@ -0,0 +1,40 @@
+<script>
+import useVuelidate from '@vuelidate/core'
+import { required, email, alpha, numeric } from '@vuelidate/validators'
+import axios from 'axios'
+const apiURL = import.meta.env.VITE_ROOT_API
+</script>
+<template>
+    <main>
+        <div>
+        <h1>Login Page</h1>
+        <form @submit.prevent="login">
+            <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="email" required />
+            </div>
+            <div>
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password" required />
+            </div>
+            <button type="submit">Login</button>
+        </form>
+        </div>
+    </main>    
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+      };
+    },
+    methods: {
+      login() {
+ 
+      },
+    },
+  };
+  </script>
